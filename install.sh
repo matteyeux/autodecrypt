2e2dca66c9e4467df858304185ba57e282911566
mathieu <mathieu.hautebas@gmail.com>
2019-05-24 14:00:12 +0200

    update script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-TOOL=autodecrypt
-
 pip3 install -r requirements.txt
-sudo install -v $TOOL *.py /usr/local/bin/
+
+sudo install -v autodecrypt/*.py /usr/local/bin/
+
+sudo rm -f /usr/local/bin/autodecrypt
+sudo ln -s /usr/local/bin/autodecrypt.py /usr/local/bin/autodecrypt
